2e66dbecf6833c1bbdacfd076020e6515fe7202b
NguyenHoangLe0701 <nguyenhoangle070105@gmail.com>
2025-02-11 19:09:43 +0700

    NguyenHoangLe-Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
     path('add_record/', views.add_record, name='add_record'),
     path('update_record/<int:pk>', views.update_record, name='update_record'),
+    path('home_dashboard/', views.home_dashboard, name='home_dashboard'),
     #left-menu
     path('tuition_debt/', views.tuition_debt, name='tuition_debt'),
 ]
